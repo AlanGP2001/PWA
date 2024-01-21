85fec628d60a5a82cdcedc6ea2b5959ff97cffa8
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-01-21 17:41:02 -0600

    Acividad 1.0

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,22 +1,8 @@
-// Definición de una función que simula una operación con un tiempo de espera (timeout)
-const ejecutarConTimeout = (ms) => {
-    return new Promise((resolve, reject) => {
-        // Simula una operación que toma cierto tiempo utilizando setTimeout
-        setTimeout(() => {
-            // Resuelve la promesa después del tiempo especificado
-            resolve(`Operación completa después de ${ms} milisegundos`);
-        }, ms);
-    });
-};
+function imprimirNumeroMasUno(numero) {
+    // Incrementa el número en uno y lo imprime en la consola
+    console.log(numero);
+    console.log(numero + 1);
+}
 
-// Uso de la función con timeout
-const tiempoLimite = 2000; // Tiempo límite en milisegundos
-ejecutarConTimeout(tiempoLimite)
-    .then((resultado) => {
-        // En caso de éxito, imprime el resultado en la consola
-        console.log(resultado);
-    })
-    .catch((error) => {
-        // En caso de error (que no ocurrirá aquí), imprime el mensaje de error en la consola
-        console.log(error);
-    });
+// Ejemplo de uso de la función
+imprimirNumeroMasUno(5); // Esto imprimirá "6" en la consola
