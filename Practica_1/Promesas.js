5b95b9f6158d81521d2221d3477ef6799a0e0135
Alan Gallardo Peñaloza <93557645+AlanGP2001@users.noreply.github.com>
2024-01-21 17:32:07 -0600

    Update Promesas.js

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,11 +1,14 @@
+// Creación de una nueva promesa que simula un rechazo inmediato con un mensaje de error
 let promesaExitosa = new Promise((resolve, reject) => {
     // Simulamos un error durante la operación
     reject("La promesa fue rechazada");
 });
 
-// Manejar la promesa con error
+// Manejar la promesa con error utilizando el método then (caso de éxito) y catch (caso de error)
 promesaExitosa.then((mensaje) => {
+    // En caso de éxito (que no ocurrirá aquí debido al rechazo), imprime el mensaje en la consola
     console.log(mensaje);
 }).catch((error) => {
+    // En caso de error, imprime el error en la consola
     console.log(error);
 });
