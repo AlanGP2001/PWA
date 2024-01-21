f96b411ad0b4e3673f8321e8f7f718630a727a1c
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-01-21 17:53:06 -0600

    Acividad 1.2

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,15 +1,11 @@
-// Definición de una función asíncrona
-const funcionAsincrona = async () => {
-    // Simula una operación asíncrona, por ejemplo, una espera de 2 segundos
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    // Después de la espera, realiza alguna acción
-    console.log("Operación asíncrona completada");
+// Definición de una función asíncrona para imprimir el conteo
+const imprimirConteo = async () => {
+    for (let i = 1; i <= 10; i++) {
+        // Espera 800 milisegundos antes de imprimir el número actual
+        await new Promise(resolve => setTimeout(resolve, 800));
+        console.log(i);
+    }
 };
 
-// Llamada a la función asíncrona
-funcionAsincrona();
-
-// Este mensaje se imprimirá inmediatamente después de la llamada a la función,
-// ya que la función asíncrona no bloquea la ejecución del código.
-console.log("Fin del programa (antes de que la operación asíncrona se complete)");
+// Llamada a la función para iniciar el conteo
+imprimirConteo();
