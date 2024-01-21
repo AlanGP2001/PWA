a3d00eaaac4012be301d51d5774ededd0b2e68f9
Alan Gallardo Peñaloza <93557645+AlanGP2001@users.noreply.github.com>
2024-01-21 17:35:02 -0600

    Update Promesas.js

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,17 +1,22 @@
+// Definición de una función que simula una operación con un tiempo de espera (timeout)
 const ejecutarConTimeout = (ms) => {
     return new Promise((resolve, reject) => {
-        // Simula una operación que toma cierto tiempo
+        // Simula una operación que toma cierto tiempo utilizando setTimeout
         setTimeout(() => {
-            resolve(`Operacion completa despues de ${ms} milisegundos`);
+            // Resuelve la promesa después del tiempo especificado
+            resolve(`Operación completa después de ${ms} milisegundos`);
         }, ms);
     });
 };
 
 // Uso de la función con timeout
-const tiempoLimite = 2000;
+const tiempoLimite = 2000; // Tiempo límite en milisegundos
 ejecutarConTimeout(tiempoLimite)
     .then((resultado) => {
+        // En caso de éxito, imprime el resultado en la consola
         console.log(resultado);
-    }).catch((error) => {
+    })
+    .catch((error) => {
+        // En caso de error (que no ocurrirá aquí), imprime el mensaje de error en la consola
         console.log(error);
     });
