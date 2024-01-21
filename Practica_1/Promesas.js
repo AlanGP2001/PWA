afc15192646b4319e111ba83123f4e4009928a00
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-01-21 17:59:45 -0600

    Acividad 1.4

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,23 +1,21 @@
-// Definición de una función asíncrona para imprimir el conteo con manejo de error
-const imprimirConteoConError = async () => {
-    try {
-        for (let i = 1; i <= 10; i++) {
-            // Espera 800 milisegundos antes de imprimir el número actual
-            await new Promise(resolve => setTimeout(resolve, 800));
-
-            // Verifica si el número actual es mayor que 8 y muestra un mensaje de error
-            if (i > 8) {
-                throw new Error("Error: La iteración superó el valor 8");
-            }
-
-            // Imprime el número actual
-            console.log(i);
-        }
-    } catch (error) {
-        // Captura el error y muestra el mensaje de error en la consola
-        console.error(error.message);
-    }
+// Función asíncrona que simula una tarea
+const tareaAsincrona = async (nombre) => {
+    console.log(`Inicio de la tarea ${nombre}`);
+    // Simula una espera de 2 segundos
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(`Fin de la tarea ${nombre}`);
 };
 
-// Llamada a la función para iniciar el conteo con manejo de error
-imprimirConteoConError();
+// Función principal que ejecuta las tareas de manera asíncrona
+const ejecutarTareasAsincronas = async () => {
+    console.log("Inicio del proceso");
+
+    // Usa await para ejecutar las tareas de manera secuencial
+    await tareaAsincrona("A");
+    await tareaAsincrona("B");
+
+    console.log("Fin del proceso");
+};
+
+// Llamada a la función principal para iniciar el proceso
+ejecutarTareasAsincronas();
