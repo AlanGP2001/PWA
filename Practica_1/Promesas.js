88b073290ee4f6e5b589dd502328b0d9be7f4195
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-01-21 17:56:48 -0600

    Acividad 1.3

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,11 +1,23 @@
-// Definición de una función asíncrona para imprimir el conteo
-const imprimirConteo = async () => {
-    for (let i = 1; i <= 10; i++) {
-        // Espera 800 milisegundos antes de imprimir el número actual
-        await new Promise(resolve => setTimeout(resolve, 800));
-        console.log(i);
+// Definición de una función asíncrona para imprimir el conteo con manejo de error
+const imprimirConteoConError = async () => {
+    try {
+        for (let i = 1; i <= 10; i++) {
+            // Espera 800 milisegundos antes de imprimir el número actual
+            await new Promise(resolve => setTimeout(resolve, 800));
+
+            // Verifica si el número actual es mayor que 8 y muestra un mensaje de error
+            if (i > 8) {
+                throw new Error("Error: La iteración superó el valor 8");
+            }
+
+            // Imprime el número actual
+            console.log(i);
+        }
+    } catch (error) {
+        // Captura el error y muestra el mensaje de error en la consola
+        console.error(error.message);
     }
 };
 
-// Llamada a la función para iniciar el conteo
-imprimirConteo();
+// Llamada a la función para iniciar el conteo con manejo de error
+imprimirConteoConError();
