ec6b9c2e6fc07216d126db50fa8057033d188ada
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-01-22 17:48:53 -0600

    Fech 4

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,7 +1,15 @@
-fetch('https://wikipedia.org')
-    .then(res => res.text())
-    .then(html => {
-        document.open();
-        document.write(html);
-        document.close();
-    })
+let user = {
+    name: 'Alan',
+    age: 22
+}
+
+fetch('https://reqres.in/api/users', {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+.then(res => res.json())
+.then(res => console.log(res))
+.catch(error => console.error('Error del Fech:', error))
