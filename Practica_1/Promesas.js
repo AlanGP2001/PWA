650ec37b813833d7c414cfba626c3db81414245b
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-01-22 18:41:31 -0600

    Fech 5

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,15 +1,13 @@
-let user = {
-    name: 'Alan',
-    age: 22
-}
+// Fetch archivos locales
 
-fetch('https://reqres.in/api/users', {
-    method: 'POST',
-    body: JSON.stringify(user),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-.then(res => res.json())
-.then(res => console.log(res))
-.catch(error => console.error('Error del Fech:', error))
+// Almacena de manera local una imagen dentro de este proyecto en la carpeta "img" realiza una solicitud fetch a esa umagen y muestra la imagen en el navegador
+
+fetch('img/Reck_and_Morty.jpeg')
+  .then(response => response.blob())
+  .then(image => {
+    let outside = URL.createObjectURL(image);
+    let imgElement = document.createElement('img');
+    imgElement.src = outside;
+    document.body.appendChild(imgElement);
+  })
+  .catch(error => console.error('Error:', error));
