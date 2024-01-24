70b3c4318d95782f061fd041ba55c42c3ead57c9
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-01-24 17:13:27 -0600

    Fech 6

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,13 +1,4 @@
-// Fetch archivos locales
-
-// Almacena de manera local una imagen dentro de este proyecto en la carpeta "img" realiza una solicitud fetch a esa umagen y muestra la imagen en el navegador
-
-fetch('img/Reck_and_Morty.jpeg')
-  .then(response => response.blob())
-  .then(image => {
-    let outside = URL.createObjectURL(image);
-    let imgElement = document.createElement('img');
-    imgElement.src = outside;
-    document.body.appendChild(imgElement);
-  })
-  .catch(error => console.error('Error:', error));
+// Solicitud a una API con fecth
+fetch('https://fakestoreapi.com/products/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
