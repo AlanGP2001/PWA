bf745414e5972c48c7272acb646a92ef71274ba7
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-01-24 17:18:14 -0600

    Fech 6.1

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,4 +1,12 @@
 // Solicitud a una API con fecth
 fetch('https://fakestoreapi.com/products/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
+  .then(response => {
+    response.json()
+      .then(data => {
+        console.log(data);
+      })
+      response.json()
+      .then(data => {
+        console.log(data);
+      })
+  })
