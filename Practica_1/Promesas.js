2dedaffcd76678a7a964e0babd8052a75f20d934
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-01-24 17:46:37 -0600

    Fech 7.2

--- Practica_1/Promesas.js
+++ Practica_1/Promesas.js
@@ -1,8 +1,13 @@
 // Manejo de errores
-fetch('https://fakestoreapi.com/products/-1')
+fetch('https://fakestoreapi.com/products/1')
   .then(response => {
-    response.json().then(console.log)
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error('Respuesta no valida')
+    }
   })
+  .then(console.log)
   .catch(error => {
     console.log('Error de la peticion' + error)
   })
