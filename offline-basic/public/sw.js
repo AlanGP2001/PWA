25ba1604badc6988194c264711c482dbf859b6bd
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-02-07 17:53:01 -0600

    Offline HTML

--- offline-basic/public/sw.js
+++ offline-basic/public/sw.js
@@ -1,9 +1,23 @@
 self.addEventListener('fetch', event => {
 
     const offlineResp = new Response(`
-        Bienvenido a mi página web.
-        Para ver el contenido, necesitas una conexión a internet.
-    `)
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Mi Página Web</title>
+        </head>
+        <body>
+            <h1>Bienvenido a mi página web Offline</h1>
+            <p>Para ver el contenido, necesitas una conexión a internet.</p>
+        </body>
+        </html>
+    `, {
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    });
 
     const resp = fetch(event.request)
         .catch(() => offlineResp)
