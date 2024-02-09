662fc4f1be11170cc60633e6778e996a2dae7d75
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-02-09 17:47:53 -0600

    Introducion ala cache

--- offline-basic/public/js/app.js
+++ offline-basic/public/js/app.js
@@ -1,3 +1,38 @@
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js')
 }
+if (window.caches) {
+    // console.log('caches supported');
+
+    // Crear primer cache
+    // caches.open('Test-1');
+    // caches.open('Test-2');
+
+    // Verificar si existe un cache
+    // caches.has('Test-3').then(console.log);
+
+    // Eliminar un cache
+    caches.delete('Test-2').then(console.log);
+
+    // Guardar un archivo en el cache
+    caches.open('Cache-v1').then(cache => {
+        // Agregar un solo archivo
+        cache.add('index.html');
+
+        // Agregar varios archivos
+        cache.addAll([
+            'logo192.png',
+            'logo512.png',
+            '/pages/offline.html',
+        ]).then(() => {
+            // borra un archivo del cache
+            cache.delete('logo192.png');
+        });
+
+        // Verificar si existe un archivo en el cache
+        cache.match('index.html').then(res => {
+            res.text().then(console.log);
+        });
+
+    });
+}
--- offline-basic/public/sw.js
+++ offline-basic/public/sw.js
@@ -1,26 +1,9 @@
 self.addEventListener('fetch', event => {
 
-    const offlineResp = new Response(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Mi Página Web</title>
-        </head>
-        <body>
-            <h1>Bienvenido a mi página web Offline</h1>
-            <p>Para ver el contenido, necesitas una conexión a internet.</p>
-        </body>
-        </html>
-    `, {
-        headers: {
-            'Content-Type': 'text/html'
-        }
-    });
+    const offlineResponse = fetch('pages/offline.html');
 
     const resp = fetch(event.request)
-        .catch(() => offlineResp)
+        .catch(() => offlineResponse);
 
-    event.respondWith(resp)
+        event.respondWith(resp);
 });
