878999b402f6afe9d1d47ace643b16e1e8c37585
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2024-02-12 17:46:45 -0600

    App Shell

--- offline-basic/public/js/app.js
+++ offline-basic/public/js/app.js
@@ -1,38 +1,3 @@
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js')
 }
-if (window.caches) {
-    // console.log('caches supported');
-
-    // Crear primer cache
-    // caches.open('Test-1');
-    // caches.open('Test-2');
-
-    // Verificar si existe un cache
-    // caches.has('Test-3').then(console.log);
-
-    // Eliminar un cache
-    caches.delete('Test-2').then(console.log);
-
-    // Guardar un archivo en el cache
-    caches.open('Cache-v1').then(cache => {
-        // Agregar un solo archivo
-        cache.add('index.html');
-
-        // Agregar varios archivos
-        cache.addAll([
-            'logo192.png',
-            'logo512.png',
-            '/pages/offline.html',
-        ]).then(() => {
-            // Interceptar un archivo por otro
-            cache.put('index.html', new Response('Interceptado por otro archivo'));
-        });
-
-        // Obtener todos los archivos del cache
-        cache.keys().then(keys => {
-            console.log(keys);
-        });
-
-    });
-}
--- offline-basic/public/sw.js
+++ offline-basic/public/sw.js
@@ -1,9 +1,21 @@
+self.addEventListener('install', event => {
+
+    const cacheAppShell = caches.open('cache-1').then(cache => {
+        return cache.addAll([
+            '/',
+            '/index.html',
+            '/js/app.js',
+            '/sw.js',
+            'static/js/bundle.js',
+            'favicon.ico',
+        ]);
+    });
+
+    event.waitUntil(cacheAppShell);
+});
+
 self.addEventListener('fetch', event => {
-
-    const offlineResponse = fetch('pages/offline.html');
-
-    const resp = fetch(event.request)
-        .catch(() => offlineResponse);
-
-        event.respondWith(resp);
+    // Cache Only, todo es servido desde el cache
+    
+    event.respondWith(caches.match(event.request));
 });
